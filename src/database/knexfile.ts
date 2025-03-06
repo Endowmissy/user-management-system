2bc0e3c8fc4da8295f8860c467e0fa2739c1cf28
Ayomikun <adeolaayo31@yahoo.com>
2025-03-06 16:28:55 +0100

    made changes

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,50 +1,52 @@
-import dotenv from 'dotenv'
-import path from 'path';
-import { attachPaginate } from 'knex-paginate'
-attachPaginate()
+import dotenv from "dotenv";
+import path from "path";
+import { attachPaginate } from "knex-paginate";
+attachPaginate();
 
-dotenv.config()
+dotenv.config();
 
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname, "dev.sqlite3")
+      filename: path.resolve(__dirname, "dev.sqlite3"),
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./migrations"
+      directory: "./migrations",
     },
     seeds: {
-      directory: "./seeds"
-    }
+      directory: "./seeds",
+    },
   },
 
   test: {
     client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname,"./test.sqlite3")
+      filename: path.resolve(__dirname, "./test.sqlite3"),
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./migrations"
+      directory: "./migrations",
     },
     seeds: {
-      directory: "./seeds"
-    }
+      directory: "./seeds",
+    },
   },
 
   production: {
     client: "sqlite3",
     connection: {
-      filename: path.resolve(__dirname, "database", "./prod.sqlite3")
+      filename: process.env.DATABASE_URL
+        ? path.resolve(__dirname, "..", process.env.DATABASE_URL)
+        : path.resolve(__dirname, "..", "database", "./prod.sqlite3"),
     },
     useNullAsDefault: true,
     migrations: {
-      directory: "./migrations"
+      directory: "./migrations",
     },
     seeds: {
-      directory: "./seeds"
-    }
-  }
+      directory: "./seeds",
+    },
+  },
 };
